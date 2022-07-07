16cdaeb06b2a01034405f4f91a91814cfedb198f
Your Name <marke1986@hotmail.com>
2022-07-07 17:51:50 +0400

    initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Monster Builder</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Bootstrap 5 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <!-- FontAwesome -->
+        <script defer src="https://use.fontawesome.com/releases/v6.1.1/js/all.js" integrity="sha384-xBXmu0dk1bEoiwd71wOonQLyH+VpgR1XcDH3rtxrLww5ajNTuMvBdL5SOiFZnNdp" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+        <div class="px-4 py-5 my-5 text-center">
+            <img class="d-block mx-auto mb-4" src="https://unavatar.now.sh/github/munazzil4" alt="" width="80" height="80">
+            <h1 class="display-5 fw-bold">Monster Builder</h1>
+            <div class="col-lg-6 mx-auto">
+              <p class="lead mb-4">
+                This is basic example using PHP Interfaces in Laravel to adhere to SOLID design princpals.
+            </p>
+              <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <a href="https://github.com/mark-stack/monster-builder-interface-example" class="btn btn-primary btn-lg px-4 gap-3" style="background-color:#24292f" target="_blank">
+                    <i class="fa-brands fa-github"></i> Github Repo
+                </a>
+              </div>
+            </div>
+          </div>
+    </body>
+
+    <!-- Bootstrap 5 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</html>
